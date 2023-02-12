e6a508ed434447cc9b82bb37270535d02ba9045d
krisito2000 <83059971+krisito2000@users.noreply.github.com>
2023-02-12 15:43:26 +0200

    Fixing the issue with the datebase. This commit adds: - New datebase (FisheriesAgencyDB) in Controller folder - registerPage form

---
+++ View/registerPage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FisheriesAgency.View
+{
+    public partial class registerPage : Form
+    {
+        public registerPage()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
