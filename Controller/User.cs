e5e6caa87216c97b4a6383a6e1018a36f34ed38c
krisito2000 <83059971+krisito2000@users.noreply.github.com>
2023-02-13 19:08:43 +0200

    Added in datebase username and password
    - Added User.cs
    - Connection to datebase

---
+++ Controller/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace FisheriesAgency.Controller
+{
+    internal class User
+    {
+        static string GetUser(string username)
+        {
+            string password = "";
+            using (SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\krisi\source\repos\FisheriesAgency\Controller\FisheriesAgencyDB.mdf;Integrated Security=True"))
+            {
+                con.Open();
+                string sqlquery = "SELECT * FROM Users WHERE username = @UName";
+                SqlCommand command = new SqlCommand(sqlquery, con);
+
+                command.Parameters.AddWithValue("@UName", username);
+                SqlDataReader sReader = command.ExecuteReader();
+
+                while (sReader.Read())
+                {
+                    username = sReader["username"].ToString();
+                    password = sReader["password"].ToString();
+                }
+                con.Close();
+            }
+            return password;
+        }
+    }
+}
---
+++ Controller/dbo.Table.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[UserID] INT NOT NULL PRIMARY KEY, 
+    [Username] NCHAR(50) NOT NULL, 
+    [Password] NCHAR(50) NOT NULL
+)
