f8b4b3026b446fe072a4897d9af1d929f6d1735b
Dimitar-Dimitrovv <ml05tko@gmail.com>
2023-02-27 06:01:56 +0200

    Model

---
+++ Model/FisheriesContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace FisheriesAgency.Model
+{
+    public class FisheriesContext : FisheriesAgencyDB
+    {
+        public FisheriesContext(FisheriesAgencyDBOptions<FisheriesContext> options): base(options)
+        {
+        }
+    }
+}
