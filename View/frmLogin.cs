7cc1b15668adcce69dd64955abda6fed94d07c38
krisito2000 <83059971+krisito2000@users.noreply.github.com>
2023-03-01 11:24:28 +0200

    isAdmin check has beed added
    - New column has been added "isAdministrator"
    - Check for user if it is administrator

--- View/frmLogin.cs
+++ View/frmLogin.cs
@@ -40,7 +40,7 @@ namespace FisheriesAgency
             if (username == string.Empty && password == string.Empty)
             {
                 MessageBox.Show("Please enter your username and password");
-            } 
+            }
             else if (username == string.Empty)
             {
                 MessageBox.Show("Please enter your username");
@@ -51,26 +51,39 @@ namespace FisheriesAgency
             }
             else
             {
-                SqlConnection connection = new SqlConnection(Program.connectionString);
-                SqlCommand command = new SqlCommand("SELECT COUNT(*) FROM [User] WHERE Username = @username AND Password = @password", connection);
-
-                command.Parameters.AddWithValue("@username", username);
-                command.Parameters.AddWithValue("@password", password);
-
-                connection.Open();
-                int result = (int)command.ExecuteScalar();
-                connection.Close();
-
-                if (result > 0)
+                using (SqlConnection connection = new SqlConnection(Program.connectionString))
                 {
-                    this.Hide();
-                    frmAdminsPanel frmAdminsPanel = new frmAdminsPanel();
-                    frmAdminsPanel.Show();
-                    MessageBox.Show("Login successful!");
-                }
-                else
-                {
-                    MessageBox.Show("Invalid username or password. Please try again.");
+                    SqlCommand command = new SqlCommand("SELECT isAdministrator FROM [User] WHERE Username = @username AND Password = @password", connection);
+
+                    command.Parameters.AddWithValue("@username", username);
+                    command.Parameters.AddWithValue("@password", password);
+
+                    connection.Open();
+                    object isAdminObj = command.ExecuteScalar();
+                    connection.Close();
+
+                    if (isAdminObj != null && isAdminObj != DBNull.Value)
+                    {
+                        bool isAdmin = (bool)isAdminObj;
+                        if (isAdmin)
+                        {
+                            this.Hide();
+                            frmAdminsPanel frmAdminsPanel = new frmAdminsPanel();
+                            frmAdminsPanel.Show();
+                            MessageBox.Show("Login successful as Administrator!");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Login successful as User!");
+                            //this.Hide();
+                            //frmUser frmUser = new frmUser();
+                            //frmUser.Show();
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid username or password. Please try again.");
+                    }
                 }
             }
         }
