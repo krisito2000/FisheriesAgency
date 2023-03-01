d8f0bfbcc9a4a0aa9348a2b4c3b05fee32164d64
Денислав Сунгов <ds70063937@edu.mon.bg>
2023-03-01 19:50:43 +0200

    From Denkata: Added Design in AdminsPanel and created frmUser

--- View/frmAdminsPanel.Designer.cs
+++ View/frmAdminsPanel.Designer.cs
@@ -28,12 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btncreate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.txtRegister = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btncreate
+            // 
+            this.btncreate.Location = new System.Drawing.Point(35, 165);
+            this.btncreate.Name = "btncreate";
+            this.btncreate.Size = new System.Drawing.Size(123, 58);
+            this.btncreate.TabIndex = 2;
+            this.btncreate.Text = "Create ";
+            this.btncreate.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(35, 266);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(123, 68);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.Location = new System.Drawing.Point(35, 88);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(83, 23);
+            this.txtLogin.TabIndex = 4;
+            this.txtLogin.Text = "Login";
+            // 
+            // txtRegister
+            // 
+            this.txtRegister.Location = new System.Drawing.Point(35, 39);
+            this.txtRegister.Name = "txtRegister";
+            this.txtRegister.Size = new System.Drawing.Size(83, 23);
+            this.txtRegister.TabIndex = 5;
+            this.txtRegister.Text = "Register";
+            // 
+            // frmAdminsPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtRegister);
+            this.Controls.Add(this.txtLogin);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btncreate);
+            this.Name = "frmAdminsPanel";
             this.Text = "frmAdminsPanel";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private Button btncreate;
+        private Button btnDelete;
+        private TextBox txtLogin;
+        private TextBox txtRegister;
     }
 }
