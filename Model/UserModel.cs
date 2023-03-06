b34aabb12a38ab46519240858ec2439c6ee1d8b5
krisito2000 <83059971+krisito2000@users.noreply.github.com>
2023-03-06 21:46:49 +0200

    Model added

---
+++ Model/UserModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace FisheriesAgency.Model
+{
+    class UserModel
+    {
+        private int id;
+        private string username;
+        private string password;
+
+        [DisplayName("ID")]
+        public int UserID { get => id; set => id = value; }
+
+        [DisplayName("Username")]
+        [Required(ErrorMessage = "Username is required.")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Username must be between 2 and 50 characters.")]
+        public string Username { get => username; set => username = value; }
+
+        [DisplayName("Password")]
+        [Required(ErrorMessage = "Password is required.")]
+        [StringLength(50, MinimumLength = 7, ErrorMessage = "Password must be between 7 and 50 characters.")]
+        public string Password { get => password; set => password = value; }
+        public UserModel()
+        {
+
+        }
+    }
+}
